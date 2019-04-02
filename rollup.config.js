11d906881223d73d5e3b5efd107b8993ea3c89f6
Conor Devine <conorjdevine@gmail.com>
2019-04-02 13:48:27 -0400

    run prettier on rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,35 @@
-import babel from "rollup-plugin-babel";
-import pkg from "./package.json";
-import { terser } from "rollup-plugin-terser";
-import localResolve from "rollup-plugin-local-resolve";
-import nodeResolve from "rollup-plugin-node-resolve";
-import replace from "rollup-plugin-replace";
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
+import localResolve from 'rollup-plugin-local-resolve';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
 
 export default [
   // CommonJS
   {
-    input: "src/index.js",
-    output: { file: "lib/react-ally.js", format: "cjs" },
+    input: 'src/index.js',
+    output: { file: 'lib/react-ally.js', format: 'cjs' },
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-    plugins: [babel()]
+    plugins: [babel(), localResolve()]
   },
   // ES
   {
-    input: "src/index.js",
-    output: { file: "es/react-ally.js", format: "es" },
+    input: 'src/index.js',
+    output: { file: 'es/react-ally.js', format: 'es' },
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-    plugins: [babel()]
+    plugins: [babel(), localResolve()]
   },
   // ES for Browsers
   {
-    input: "src/index.js",
-    output: { file: "es/react-ally.mjs", format: "es" },
+    input: 'src/index.js',
+    output: { file: 'es/react-ally.mjs', format: 'es' },
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     plugins: [
       babel(),
       nodeResolve({ jsnext: true }),
-      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      localResolve(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
       terser({
         compress: {
           pure_getters: true,
@@ -41,46 +42,46 @@ export default [
   },
   // UMD Development
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: {
-      file: "dist/react-ally.js",
-      format: "umd",
-      name: "react-ally",
+      file: 'dist/react-ally.js',
+      format: 'umd',
+      name: 'react-ally',
       globals: {
-        "body-scroll-lock": "BodyScrollLock",
-        "prop-types": "propTypes",
-        react: "React",
-        "react-dom": "ReactDOM"
+        'body-scroll-lock': 'BodyScrollLock',
+        'prop-types': 'propTypes',
+        react: 'React',
+        'react-dom': 'ReactDOM'
       }
     },
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     plugins: [
       nodeResolve({ jsnext: true }),
-      babel({ exclude: "node_modules/**" }),
+      babel({ exclude: 'node_modules/**' }),
       localResolve(),
-      replace({ "process.env.NODE_ENV": JSON.stringify("development") })
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') })
     ]
   },
   // UMD Production
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: {
-      file: "dist/react-ally.min.js",
-      format: "umd",
-      name: "react-ally",
+      file: 'dist/react-ally.min.js',
+      format: 'umd',
+      name: 'react-ally',
       globals: {
-        "body-scroll-lock": "BodyScrollLock",
-        "prop-types": "propTypes",
-        react: "React",
-        "react-dom": "ReactDOM"
+        'body-scroll-lock': 'BodyScrollLock',
+        'prop-types': 'propTypes',
+        react: 'React',
+        'react-dom': 'ReactDOM'
       }
     },
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     plugins: [
       nodeResolve({ jsnext: true }),
-      babel({ exclude: "node_modules/**" }),
+      babel({ exclude: 'node_modules/**' }),
       localResolve(),
-      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
       terser({
         compress: {
           pure_getters: true,
